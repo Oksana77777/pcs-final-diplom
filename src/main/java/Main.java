d49a8f348c414b0e89fa7172b5d3bde385f109b6
Oksana77777 <cassandra.bk@mail.ru>
2023-05-15 16:47:05 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8989)) { // стартуем сервер один(!) раз
+            BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream())
+                ) {
+                    String request = in.readLine();
+                    String answer = engine.search(request).toString();
+                    out.println(answer);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/PageEntry.java
@@ -0,0 +1,42 @@
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PageEntry implements Comparable<PageEntry> {
+    private final String pdfName;
+    private final int page;
+    private final int count;
+
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        return Integer.compare(o.getCount(), this.getCount());
+    }
+
+    @Override
+    public String toString() {
+        Map map = new LinkedHashMap();
+        map.put("pdfName", pdfName);
+        map.put("page", page);
+        map.put("count", count);
+        JSONObject result = null;
+        try {
+            result = new JSONObject(map);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result.toString();
+    }
+}
